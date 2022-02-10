a755c64c376867d5d9261feec71f4414e4ac81b8
Wille-J <82776900+Wille-J@users.noreply.github.com>
2022-02-10 20:28:21 +0100

    Created SudokuPanel class and added comments.

--- src/main/java/SudokuFrame.java
+++ src/main/java/SudokuFrame.java
@@ -1,64 +1,80 @@
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class SudokuFrame extends JFrame {
 	
-	// HeadlessException is thrown when code that is dependent on a keyboard, display, 
-	//or mouse is called in an environment that does not support a keyboard, display, or mouse.
+	private SudokuPanel sPanel;
+
+	/**
+	 * HeadlessException is thrown when code that is dependent on a keyboard,
+	 * display, or mouse is called in an environment that does not support a
+	 * keyboard, display, or mouse.
+	 */
 	public SudokuFrame() throws HeadlessException {
-		
+
 		/**
 		 * This part is for the game frame.
 		 */
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //This exits the application when frame window is closed.
-		
-		this.setTitle("Sudoku"); //Set the title of the frame to "Sudoku"
-		
-		this.setMinimumSize (new Dimension(800, 600)); //Sets the frame to start size of minimum 800x600.
-		
-		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This exits the application when frame window is closed.
+
+		this.setTitle("Sudoku"); // Set the title of the frame to "Sudoku"
+
+		this.setMinimumSize(new Dimension(800, 600)); // Sets the frame to start size of minimum 800x600.
+
 		/**
 		 * This is for the MenuBar.
 		 */
-		JMenuBar menuBar = new JMenuBar(); //Needed to create a menu bar.
-		JMenu file = new JMenu("Game"); //file variable name as in the "File"-menu in software.
-		JMenu newGame = new JMenu("New Game"); //Set the name of the new game option variable.
-		
-		JMenuItem sixBySixGame = new JMenuItem("6 by 6 Game");
+		JMenuBar menuBar = new JMenuBar(); // Needed to create a menu bar.
+		JMenu file = new JMenu("Game"); // file variable name as in the "File"-menu in software.
+		JMenu newGame = new JMenu("New Game"); // Set the name of the new game option variable.
+
+		JMenuItem sixBySixGame = new JMenuItem("6 by 6 Game"); //Sets the string name for the item variable.
 		JMenuItem nineByNineGame = new JMenuItem("9 by 9 Game");
 		JMenuItem twelveByTwelveGame = new JMenuItem("12 by 12 Game");
-		
-		
-		newGame.add(sixBySixGame);
+
+		newGame.add(sixBySixGame); //Adds the variable item to the newGame menu drop down.
 		newGame.add(nineByNineGame);
 		newGame.add(twelveByTwelveGame);
-		
-		
-		this.setJMenuBar(menuBar); //Instead of using private members and this = this in constructor?
-		menuBar.add(file); //Add the "Game" downdrop menu to the menubar.
-		file.add(newGame); //Add the "New Game" (newGame) option in the "Game" (file) menu.
-		
-		
-	}
-	
-	
 
-	//SwingUtilites is a collection of utility methods for Swing.
-	//As Swing is single threaded, invokeLater together with the Runnable(), assigns the thread to the eventThread that handles gui.
-	//(invokeLater should not be used for database searches as it blocks user input until done.)
+		this.setJMenuBar(menuBar); // Instead of using private members and this = this in constructor?
+		menuBar.add(file); // Add the "Game" drop down menu to the menu bar.
+		file.add(newGame); // Add the "New Game" (newGame) option in the "Game" (file) menu.
+		
+		
+		JPanel windowPanel = new JPanel(); 
+		windowPanel.setLayout(new FlowLayout()); //Creates a layout with a centred alignment and a default 5-unit horizontal and vertical gap.
+
+		
+		windowPanel.setPreferredSize(new Dimension(800, 900)); //? Gör inget?
+		
+		sPanel = new SudokuPanel(); //Create a object to add to our window.
+		
+		windowPanel.add(sPanel); //Adds the white panel created in SudokuPanel to the window. ?
+		
+		this.add(windowPanel); //Shows the ?
+
+	}
+
+	/**
+	 * SwingUtilites is a collection of utility methods for Swing. As Swing is
+	 * single threaded, invokeLater together with the Runnable(), assigns the thread
+	 * to the eventThread that handles gui. (invokeLater should not be used for
+	 * database searches as it blocks user input until done.)
+	 */
 	public static void main(String args[]) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				SudokuFrame frame = new SudokuFrame();
 				frame.setVisible(true);
-				
 			}
 		});
 	}
--- src/main/java/SudokuPanel.java
+++ src/main/java/SudokuPanel.java
@@ -1,4 +1,53 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.LayoutManager;
 
-public class SudokuPanel {
+import javax.swing.JPanel;
 
+public class SudokuPanel extends JPanel {
+
+	public SudokuPanel() {
+		this.setPreferredSize(new Dimension(540, 450)); // Sets the size of the white Sudoku panel background.
+	}
+
+	@Override // From JPanel get the method paintComponent.
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g); //
+		Graphics2D g2d = (Graphics2D) g; // define an object from the class Graphics2D.
+		g2d.setColor(new Color(1.0f, 1.0f, 1.0f)); // Here you set the background to white. ?
+
+		g2d.fillRect(0, 0, this.getWidth(), this.getHeight()); // ?
+
+		g2d.setColor(new Color(0.0f, 0.0f, 0.0f)); // Sets the colour of the strokes.
+
+		int slotWidth = this.getWidth() / 9; // Distance between the lines/strokes.
+		int slotHeight = this.getHeight() / 9;
+
+		for (int x = 0; x <= getWidth(); x += slotWidth) { // checks if x is greater then the width of the panel. if
+															// not, add one slot width....
+			if ((x / slotWidth) % 3 == 0) {
+				g2d.setStroke(new BasicStroke(2));
+				g2d.drawLine(x, 0, x, this.getHeight());
+			} else {
+				g2d.setStroke(new BasicStroke(1)); // sets the stroke type for the drawLine function below.
+				g2d.drawLine(x, 0, x, this.getHeight()); // Draws a line, using the current color, between the points.
+			}
+		}
+		
+		 // Same as for the vertical lines.
+		for ( int y = 0; y <= getHeight(); y += slotHeight) {
+			if ((y / slotWidth) % 3 == 0) {
+				g2d.setStroke(new BasicStroke(2));
+				g2d.drawLine(0,  y, this.getWidth(), y);
+			}
+			else {
+				g2d.setStroke(new BasicStroke(1));
+				g2d.drawLine(0,  y,  this.getWidth(), y);
+			}
+		}
+		
+	}
 }
